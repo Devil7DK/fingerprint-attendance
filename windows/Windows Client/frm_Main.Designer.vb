4f8844f70b502042d9e9bf936e0e02f2d47a667c
Devil7DK <dineshthangavel47@gmail.com>
2019-03-12 16:13:25 +0530

    windows: Initial commit for windows client

---
+++ windows/Windows Client/frm_Main.Designer.vb
@@ -0,0 +1,76 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm_Main
+    Inherits DevExpress.XtraBars.Ribbon.RibbonForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.RibbonControl = New DevExpress.XtraBars.Ribbon.RibbonControl()
+        Me.RibbonPage1 = New DevExpress.XtraBars.Ribbon.RibbonPage()
+        Me.RibbonPageGroup1 = New DevExpress.XtraBars.Ribbon.RibbonPageGroup()
+        Me.RibbonStatusBar = New DevExpress.XtraBars.Ribbon.RibbonStatusBar()
+        Me.SuspendLayout()
+        ' 
+        ' RibbonControl
+        ' 
+        Me.RibbonControl.ApplicationIcon = Nothing
+        Me.RibbonControl.Location = New System.Drawing.Point(0, 0)
+        Me.RibbonControl.Name = "RibbonControl"
+        Me.RibbonControl.Pages.AddRange(New DevExpress.XtraBars.Ribbon.RibbonPage() {Me.RibbonPage1})
+        Me.RibbonControl.SelectedPage = Me.RibbonPage1
+        Me.RibbonControl.Size = New System.Drawing.Size(442, 141)
+        ' 
+        ' RibbonPage1
+        ' 
+        Me.RibbonPage1.Groups.AddRange(New DevExpress.XtraBars.Ribbon.RibbonPageGroup() {Me.RibbonPageGroup1})
+        Me.RibbonPage1.Name = "RibbonPage1"
+        Me.RibbonPage1.Text = "RibbonPage1"
+        ' 
+        ' RibbonPageGroup1
+        ' 
+        Me.RibbonPageGroup1.Name = "RibbonPageGroup1"
+        Me.RibbonPageGroup1.Text = "RibbonPageGroup1"
+        ' 
+        ' RibbonStatusBar
+        ' 
+        Me.RibbonStatusBar.Location = New System.Drawing.Point(0, 425)
+        Me.RibbonStatusBar.Name = "RibbonStatusBar"
+        Me.RibbonStatusBar.Ribbon = Me.RibbonControl
+        Me.RibbonStatusBar.Size = New System.Drawing.Size(442, 24)
+        ' 
+        ' frm_Main
+        ' 
+        Me.ClientSize = New System.Drawing.Size(442, 449)
+        Me.Controls.Add(Me.RibbonStatusBar)
+        Me.Controls.Add(Me.RibbonControl)
+
+        Me.Ribbon = Me.RibbonControl
+        Me.StatusBar = Me.RibbonStatusBar
+        Me.Text = "frm_Main"
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ResumeLayout(False)
+
+
+    End Sub
+
+    Friend WithEvents RibbonControl As DevExpress.XtraBars.Ribbon.RibbonControl
+    Friend WithEvents RibbonPage1 As DevExpress.XtraBars.Ribbon.RibbonPage
+    Friend WithEvents RibbonPageGroup1 As DevExpress.XtraBars.Ribbon.RibbonPageGroup
+    Friend WithEvents RibbonStatusBar As DevExpress.XtraBars.Ribbon.RibbonStatusBar
+
+
+End Class
---
+++ windows/Windows Client/frm_Main.vb
@@ -0,0 +1,3 @@
+﻿Public Class frm_Main
+
+End Class
