38a43f9b274697bd1070164eb35eeec41412593d
Devil7DK <dineshthangavel47@gmail.com>
2019-03-12 16:25:15 +0530

    windows: Add Course Object

---
+++ windows/Windows Client/Objects/Course.vb
@@ -0,0 +1,28 @@
+﻿Namespace Objects
+    Public Class Course
+
+#Region "Properties"
+        Property ID As Integer
+        Property ShortName As String
+        Property FullName As String
+        Property Prefix As String
+#End Region
+
+#Region "Constructor"
+        Sub New()
+            Me.ID = 0
+            Me.ShortName = ""
+            Me.FullName = ""
+            Me.Prefix = ""
+        End Sub
+
+        Sub New(ByVal ID As Integer, ByVal ShortName As String, ByVal FullName As String, ByVal Prefix As String)
+            Me.ID = ID
+            Me.ShortName = ShortName
+            Me.FullName = FullName
+            Me.Prefix = Prefix
+        End Sub
+#End Region
+
+    End Class
+End Namespace
