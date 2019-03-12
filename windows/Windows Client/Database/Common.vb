211bf27b466261750251a5ed514529df7e03f554
Devil7DK <dineshthangavel47@gmail.com>
2019-03-12 16:58:04 +0530

    windows: Implement Common  Database Functions

---
+++ windows/Windows Client/Database/Common.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Data.SQLite
+
+Namespace Database
+    Public Module Common
+
+#Region "Variables"
+        Private Connection As SQLiteConnection
+#End Region
+
+#Region "Public Functions"
+        Public Function GetConnection() As SQLiteConnection
+            If Connection Is Nothing Then
+                Connection = New SQLiteConnection(String.Format("Data Source={0};Version=3;Pooling=True;Max Pool Size=100;", IO.Path.Combine(Application.StartupPath, "data.db")))
+            End If
+            Return Connection
+        End Function
+#End Region
+
+    End Module
+End Namespace
