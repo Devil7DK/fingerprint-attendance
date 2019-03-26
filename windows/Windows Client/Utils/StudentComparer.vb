e2d2958755bb18eb3aa559ace1a778daa410832f
Devil7DK <dineshthangavel47@gmail.com>
2019-03-26 21:50:48 +0530

    windows: Implement Student Object Comparer

---
+++ windows/Windows Client/Utils/StudentComparer.vb
@@ -0,0 +1,11 @@
+﻿Namespace Utils
+    Public Class StudentComparer : Implements IComparer(Of Objects.Student)
+
+#Region "Functions"
+        Public Function Compare(x As Objects.Student, y As Objects.Student) As Integer Implements IComparer(Of Objects.Student).Compare
+            Return x.RollNo.CompareTo(y.RollNo)
+        End Function
+#End Region
+
+    End Class
+End Namespace
